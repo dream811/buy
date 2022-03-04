b6aeaa4e231c10ab67a5f9c2df5b80923773fa81
Dream811 <dragonwinter811@outlook.com>
2022-03-04 19:28:24 +0900

    fixed

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -182,7 +182,7 @@ class Index extends BaseController {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $result = curl_exec($curl);
+        echo $result = curl_exec($curl);
         curl_getinfo($curl, CURLINFO_HTTP_CODE);
         curl_close($curl);
         $decoded_json = json_decode($result, true);
